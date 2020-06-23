70c14519e15942695babf96550646f7d8aadec03
Ross Spencer <all.along.the.watchtower2001@gmail.com>
2020-06-23 11:06:54 -0400

    Provide easier access to DB

--- src/settings/settings.py
+++ src/settings/settings.py
@@ -1,49 +1,28 @@
 import os
 
-# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+DB_DIR = os.path.dirname(os.path.dirname(BASE_DIR))
 
-
-"""
 DATABASES = {
     "default": {
-        "ENGINE": 'django.db.backends.mysql',
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": os.path.join(DB_DIR, "db", "db.sqlite3"),
+    },
+    "am": {
+        "ENGINE": "django.db.backends.mysql",
         "NAME": "MCP",
         "USER": "root",
         "PASSWORD": "12345",
         "HOST": "127.0.0.1",
         "PORT": "62001",
-    }
-}
-"""
-
-
-DATABASES = {
-    "default": {
-        # Database driver
-        "ENGINE": "django.db.backends.sqlite3",
-        # Replace below with Database Name if using other database engines
-        "NAME": os.path.join(BASE_DIR, "..", "db", "db.sqlite3"),
-    }
+    },
 }
 
+# TODO: This should silence the system check we don't need for the SQL database
+# as we only ever read from it.
+SILENCED_SYSTEM_CHECKS = ["django_mysql.W002"]
 
-
-"""
-INSTALLED_APPS = (
-    "main",
-    #"django.contrib.contenttypes",
-    #"django.contrib.auth",
-    "fpr",
-)
-"""
-
-INSTALLED_APPS = (
-    "main",
-    "fpr",
-    "django.contrib.contenttypes",
-    "django.contrib.auth",
-)
+INSTALLED_APPS = ("main", "fpr", "django.contrib.contenttypes", "django.contrib.auth")
 
 # SECURITY WARNING: Modify this secret key if using in production!
 SECRET_KEY = "6few3nci_q_o@l1dlbk81%wcxe!*6r29yu629&d97!hiqat9fa"
