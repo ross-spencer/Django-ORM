a2896ede1340b684714015c90382b242ce2b5422
Ross Spencer <all.along.the.watchtower2001@gmail.com>
2020-06-23 13:18:01 -0400

    Prettify manage.py output
    Former-commit-id: 1734f27f98190eb83715497294bd6751c55c29d6

--- src/manage.py
+++ src/manage.py
@@ -1,12 +1,16 @@
 # -*- coding: utf-8 -*-
 
-from __future__ import absolute_import
+from __future__ import absolute_import, print_function
 
 import os
 import sys
+
+THIS_DIR = os.path.dirname(os.path.abspath(__file__))
+sys.path.append(os.path.join(THIS_DIR))
 
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings.settings")
     from django.core.management import execute_from_command_line
 
     execute_from_command_line(sys.argv)
+    print("")
